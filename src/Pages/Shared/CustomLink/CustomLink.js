25109ce25ab86586b816e360990f4c964164984a
diptasahacse <diptasaha.lpu.cse@gmail.com>
2022-04-17 18:27:00 +0600

    use bootstrap navbar and use custom active link

--- src/Pages/Shared/CustomLink/CustomLink.js
+++ src/Pages/Shared/CustomLink/CustomLink.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { Link, useMatch, useResolvedPath } from 'react-router-dom';
 
-const CustomLink = () => {
+const CustomLink = ({ children, to, ...props }) => {
+
+    let resolved = useResolvedPath(to);
+    let match = useMatch({ path: resolved.pathname, end: true });
     return (
         <div>
-            
+            <Link
+                style={match && { color: "red", fontWeight: '500' }}
+                to={to}
+                {...props}
+            >
+                {children}
+            </Link>
+
         </div>
     );
 };
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import CustomLink from '../CustomLink/CustomLink';
+
 
 const Header = () => {
     return (
-        <div>
-            
-        </div>
+        <Navbar bg="light" expand="lg">
+            <Container>
+                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="ms-auto">
+                        <Nav.Link as={CustomLink} to='/'>Home</Nav.Link>
+                        <Nav.Link as={CustomLink} to='/about'>About</Nav.Link>
+                        <Nav.Link as={CustomLink} to='/blogs'>Blogs</Nav.Link>
+                        
+                        
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     );
 };
 
