aada80a9796eef833905f6a6bacff8c9a1945659
diptasahacse <diptasaha.lpu.cse@gmail.com>
2022-04-17 23:51:16 +0600

    make require auth component and design login page

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import NotFound from './Pages/Shared/NotFound/NotFound';
 import Register from './Pages/Register/Register';
 import Login from './Pages/Login/Login';
 import Checkout from './Pages/Checkout/Checkout';
+import RequireAuth from './Pages/RequireAuth/RequireAuth';
 
 function App() {
   return (
@@ -20,7 +21,11 @@ function App() {
         <Route path='/about' element={<About></About>}></Route>
         <Route path='/blogs' element={<Blogs></Blogs>}></Route>
         <Route path='/register' element={<Register></Register>}></Route>
-        <Route path='/checkout' element={<Checkout></Checkout>}></Route>
+        <Route path='/checkout' element={
+          <RequireAuth>
+            <Checkout></Checkout>
+          </RequireAuth>
+        }></Route>
         <Route path='/login' element={<Login></Login>}></Route>
         <Route path='*' element={<NotFound></NotFound>}></Route>
       </Routes>
--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,10 +1,30 @@
 import React from 'react';
+import { Button, Container, Form } from 'react-bootstrap';
 
 const Login = () => {
     return (
-        <div>
-            
-        </div>
+        <Container>
+            <div className='text-center mt-5'>
+                <h3 className='text-primary'>Please Login</h3>
+            </div>
+            <Form className='w-50 mx-auto'>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" placeholder="Enter email" />
+                    <Form.Text className="text-muted">
+                        We'll never share your email with anyone else.
+                    </Form.Text>
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Password" />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Login
+                </Button>
+            </Form>
+        </Container>
     );
 };
 
