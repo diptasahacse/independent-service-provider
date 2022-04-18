efae43b96084c233d40815025b0edb7c766def02
diptasahacse <diptasaha.lpu.cse@gmail.com>
2022-04-18 15:03:45 +0600

    design checkout page

--- src/Pages/Checkout/Checkout.js
+++ src/Pages/Checkout/Checkout.js
@@ -1,21 +1,52 @@
 import React, { useEffect, useState } from 'react';
+import { Button, Container, Form } from 'react-bootstrap';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
 import useService from '../../hooks/useService';
 
 const Checkout = () => {
-    const [serviceId, setServiceId] = useState(0)
-    useEffect(()=>{
+    const [serviceId, setServiceId] = useState(0);
+    const [user, loading, error] = useAuthState(auth);
+    useEffect(() => {
         let id = JSON.parse(localStorage.getItem('serviceid'))
         setServiceId(id)
-        
-    },[])
+
+    }, [])
+    console.log(user)
     const serviceObj = useService(serviceId);
     return (
-        <div>
-            {
-                serviceObj?.name
-            }
+        <Container>
+            <div className='border p-3 m-3 w-50 mx-auto'>
+                <h5>Service Name: {serviceObj?.name}</h5>
+                <p>Price: ${serviceObj?.price}</p>
+            </div>
+            <div className=' w-75 mx-auto border p-3 m-3'>
+                <Form>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>User Name</Form.Label>
+                        <Form.Control type="name" value={user?.displayName} disabled />
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type="email" value={user?.email} disabled />
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="formBasicAddress">
+                        <Form.Label>Address</Form.Label>
+                        <Form.Control type="text" placeholder="Enter address" />
+                    </Form.Group>
+
             
-        </div>
+                    
+                    <Button variant="primary" type="submit">
+                        Submit
+                    </Button>
+                </Form>
+            </div>
+
+
+        </Container>
     );
 };
 
